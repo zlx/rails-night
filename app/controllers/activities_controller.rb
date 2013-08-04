9124581b20a5ac1dfaa78b5cbad2e0d19c0debf1
soffolk <zlx.star@gmail.com>
2013-08-04 18:49:58 +0800

    make email notification delay

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -57,7 +57,7 @@ class ActivitiesController < ApplicationController
 
   def join
     @activity.users << current_user
-    ActivityMailer.welcome_email(@activity.decorate, current_user).deliver
+    ActivityMailer.delay.welcome_email(@activity.decorate, current_user)
     #ActivityMailer.upcoming_email(@activity.decorate).deliver
     redirect_to @activity, notice: "欢迎参加活动"
   end
