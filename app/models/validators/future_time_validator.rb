395b4f6b1702200e0baf999bfb92d2041bb66544
soffolk <zlx.star@gmail.com>
2013-08-09 00:25:16 +0800

    fix bug for update for completed activity

--- app/models/validators/future_time_validator.rb
+++ app/models/validators/future_time_validator.rb
@@ -1,6 +1,7 @@
 class FutureTimeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors.add attribute, (options[:message] || "must be a future time") unless
-    value.present? && value.in_time_zone > Time.zone.now
+    if record.new_record? && value.present? && value.in_time_zone > Time.zone.now
+      record.errors.add attribute, (options[:message] || "must be a future time")
+    end
   end
 end
