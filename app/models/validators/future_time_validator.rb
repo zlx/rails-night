d4166386d5961097641824f2f1502c9f46905714
soffolk <zlx.star@gmail.com>
2013-08-12 08:16:26 +0800

    fix future time bug

--- app/models/validators/future_time_validator.rb
+++ app/models/validators/future_time_validator.rb
@@ -1,6 +1,6 @@
 class FutureTimeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    if record.new_record? && value.present? && value.in_time_zone > Time.zone.now
+    if record.new_record? && (value.blank? || value.in_time_zone < Time.zone.now)
       record.errors.add attribute, (options[:message] || "must be a future time")
     end
   end
